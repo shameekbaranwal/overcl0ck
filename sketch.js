ac089669c10b260ad4f922eb168514a331ae7adb
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-04-10 14:12:34 +0530

    Added variable size based on windowWidth and windowHeight

--- sketch.js
+++ sketch.js
@@ -9,18 +9,31 @@ let CLOCKS_INVISIBLE = false;
 
 // let CLOCK_COLOUR = '#2B3350';
 let CLOCK_COLOUR = '#00000f';
-let BACKGROUND_COLOUR = '#00000f';
+let BACKGROUND_COLOUR = CLOCK_COLOUR;
 // let LINE_COLOUR = '#FFCAE9';
 // let LINE_COLOUR = '#75E4B3';
 let LINE_COLOUR = '#ff4d4d';
 // let LINE_COLOUR = '#FFB17A';
 
+let CANVAS_RATIO = 0.7; //if the page is opened in landscape mode (considered default)
+
 function preload() {
     clocksConfig = loadJSON('./digitConfig.json');
 }
 
+/////////////////////////////////////////////////////////
+
 function setup() {
-    canv = createCanvas(800, 600);
+    if (windowHeight >= windowWidth) //if the page is opened in portrait mode
+        CANVAS_RATIO = 0.9;
+    
+    canv = createCanvas(windowWidth * CANVAS_RATIO, 0.75 * windowWidth * CANVAS_RATIO);
+    
+    if (windowHeight <= windowWidth && windowHeight <= 0.75 * windowWidth) {
+        // CANVAS_RATIO = 0.9;
+        resizeCanvas(windowHeight * 1.33 * CANVAS_RATIO, windowHeight * CANVAS_RATIO);
+    }
+
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2;
     canv.position(x, y);
@@ -29,6 +42,10 @@ function setup() {
 
 function draw() {
     background(BACKGROUND_COLOUR);
+    noFill();
+    stroke(240);
+    strokeWeight(0.5);
+    rect(0, 0, width - 1, height - 1);
 
     drawHours();
     drawMinutes();
@@ -61,16 +78,16 @@ function changeClockVisbility() {
 
 function setupHours() {
     let config = {
-        x: 50,
-        y: 50,
-        size: 27,
+        x: normalize(50),
+        y: normalize(50),
+        size: normalize(27),
         rows: 10,
         columns: 6,
         speed: 2.7, //right now, the lower this is, the faster the animation gets. Will refactor later.
     }
     // hr = 17; //debugging
     hrDigits[0] = new Digit(config); //10s digit of hour
-    config.x = config.x + config.columns * config.size + 15;
+    config.x = config.x + config.columns * config.size + normalize(15);
     hrDigits[1] = new Digit(config); //1s digit of hour
 }
 
@@ -84,9 +101,9 @@ function drawHours() {
 
 function setupMinutes() {
     let config = {
-        x: 440,
-        y: 50,
-        size: 27,
+        x: normalize(440),
+        y: normalize(50),
+        size: normalize(27),
         rows: 10,
         columns: 6,
         speed: 2, //right now, the lower this is, the faster the animation gets. Will refactor later.
@@ -94,7 +111,7 @@ function setupMinutes() {
 
     // min = 52; // debugging
     minDigits.push(new Digit(config)); //10s digit of min
-    config.x = config.x + config.columns * config.size + 15;
+    config.x = config.x + config.columns * config.size + normalize(15);
     minDigits.push(new Digit(config)); //1s digit of min
 }
 
@@ -108,9 +125,9 @@ function drawMinutes() {
 
 function setupSeconds() {
     let config = {
-        x: 240,
-        y: 330,
-        size: 27,
+        x: normalize(240),
+        y: normalize(330),
+        size: normalize(27),
         rows: 10,
         columns: 6,
         speed: 0.5, //right now, the lower this is, the faster the animation gets. Will refactor later.
@@ -118,7 +135,7 @@ function setupSeconds() {
 
     // sec = 19; // debugging
     secDigits.push(new Digit(config)); //10s digit of sec
-    config.x = config.x + config.columns * config.size + 15;
+    config.x = config.x + config.columns * config.size + normalize(15);
     secDigits.push(new Digit(config)); //1s digit of sec
 }
 
@@ -128,4 +145,8 @@ function drawSeconds() {
     secDigits[0].show(i);
     i = clocksConfig.labels.indexOf(sec % 10 + '');
     secDigits[1].show(i);
+}
+
+function normalize(value) {
+    return width * (value / 800);
 }
